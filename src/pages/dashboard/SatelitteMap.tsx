1c292b0c205d99f0ca168b46295d70df2ee407c3
killer010599124 <122247280+killer010599124@users.noreply.github.com>
2023-05-16 09:39:04 -0400

    a

--- src/pages/dashboard/SatelitteMap.tsx
+++ src/pages/dashboard/SatelitteMap.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useRef, useState } from 'react';
-import { useEffect } from 'react';
+import { useRef, useState, } from 'react';
+import { useEffect, } from 'react';
 import { initMap } from './initMap';
 import { useMap } from './useMap';
 import assets from '../../assets';
@@ -8,13 +8,15 @@ import "./style.css"
 
 type Props = {};
 
+
 const SatelitteMap = (props: Props) => {
 
   const mapRef = useRef<HTMLDivElement>(null);
   const [dataVisible, setDataVisible] = useState(1);
- 
+
   const [lng, setLng] = useState(0);
   const [lat, setLat] = useState(0);
+
 
   const handleLongtitude = (num: number) => {
     // 👇️ take the parameter passed from the Child component
@@ -29,7 +31,8 @@ const SatelitteMap = (props: Props) => {
     // console.log('argument from Child: ', lng);
   };
 
-  useMap(mapRef, "mapbox://styles/mapbox/satellite-streets-v12", handleLongtitude,handleLatitude)
+
+  useMap(mapRef, "mapbox://styles/mapbox/satellite-streets-v12", handleLongtitude, handleLatitude)
 
   return (
     <>
@@ -45,10 +48,10 @@ const SatelitteMap = (props: Props) => {
           borderColor: "white",
           borderWidth: "5px"
         }}
-        onClick={() => {
-          if(dataVisible) setDataVisible(0);
-          else setDataVisible(1);
-        }}
+          onClick={() => {
+            if (dataVisible) setDataVisible(0);
+            else setDataVisible(1);
+          }}
         >
           <img src={assets.images.data} style={{ width: "100%", height: "100%" }} />
         </button>
@@ -59,12 +62,12 @@ const SatelitteMap = (props: Props) => {
         marginTop: "-1%",
         marginRight: "2%", zIndex: "1"
       }}>
-        <form style={dataVisible ? {display : "none"} : {display : "block"}}>
+        <form style={dataVisible ? { display: "none" } : { display: "block" }}>
           <div style={{
             display: 'flex',
             flexDirection: 'row',
             justifyContent: "space-evenly",
-            paddingTop : "25px"
+            paddingTop: "25px"
           }}>
             <input type="text" placeholder="Name" />
             <input type="date" placeholder="Date" />
@@ -74,25 +77,70 @@ const SatelitteMap = (props: Props) => {
             flexDirection: 'row',
             justifyContent: "space-evenly"
           }}>
-            <input type="text" placeholder="Longtitude" value={lng}/>
-            <input type="text" placeholder="Latitude" value={lat}/>
+            <input type="text" placeholder="Longtitude" value={lng} />
+            <input type="text" placeholder="Latitude" value={lat} />
           </div>
           <textarea placeholder="Description"></textarea>
           <div style={{
             display: 'flex',
             flexDirection: 'row',
             justifyContent: "space-evenly",
-            paddingTop : "5px"
+            paddingTop: "5px"
           }}>
-            <input type = "button"  value={"Add"} />
-            <input type = "button"  value={"Edit"} />
-            <input type = "button"  value={"Delete"} />
+            <input type="button" value={"Add"} />
+            <input type="button" value={"Edit"} />
+            <input type="button" value={"Delete"} />
           </div>
-          
+
         </form>
       </div>
-
-
+      <div style={dataVisible ? { display: "none" } : { 
+        display: "block",
+        position: "absolute",
+        right: "0px",
+        marginTop: "280px",
+        marginRight: "70px", zIndex: "1",
+        background : "black",
+        opacity : "0.75",
+        color : "white",
+        width : "345px",
+        height : "60%",
+        padding : "5px",
+        borderRadius:"10px" }
+        
+      }>
+        <table style={{
+          textAlign : "center",
+          width : "100%",
+          height : "100%"
+        }}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Country</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>John</td>
+            <td>25</td>
+            <td>USA</td>
+          </tr>
+          <tr>
+            <td>Jane</td>
+            <td>30</td>
+            <td>Canada</td>
+          </tr>
+          <tr>
+            <td>Bob</td>
+            <td>35</td>
+            <td>Australia</td>
+          </tr>
+        </tbody>
+      </table>
+      </div>
+      
       <div ref={mapRef} className='map' style={{ padding: "0px !important", height: "94%", width: "100%" }} />
 
     </>
